6edd00e3deaf5d31144babfdaa7337c65702490a
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-24 01:32:40 +0530

    Navigated to the login activity from the splash activity.

---
+++ app/src/main/java/com/gmail/apigeoneer/motionlayoutdemo/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.apigeoneer.motionlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/gmail/apigeoneer/motionlayoutdemo/SplashActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/motionlayoutdemo/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.gmail.apigeoneer.motionlayoutdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({ 
+            /* Create an Intent that will start the Menu-Activity. */
+            val mainIntent = Intent(this, LoginActivity::class.java)
+            this.startActivity(mainIntent)
+
+            //overriding the default sliding animation transition to the new activity
+
+            // remove the Splash Activity from the stack (since we only want to show it once i.e., at the v beginning)
+            this.finish()
+        }, 1000)
     }
 }
